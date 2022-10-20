6f4c12a52e1c7a9a4f882f2e36b1e8e609685358
KateKo04 <kovalkatty469@gmail.com>
2022-10-20 10:50:16 +0300

    message

--- src/main/java/org/example/FlowerBucket.java
+++ src/main/java/org/example/FlowerBucket.java
@@ -2,13 +2,27 @@ package org.example;
 
 import java.util.ArrayList;
 
-public class FlowerBucket {
-    private static ArrayList<FlowerPack> bucket = new ArrayList<>();
+/**
+ * bucket of flowers
+ */
 
+public class FlowerBucket {
+
+    /**
+     * initialize bucket of flowers
+     */
+    private static final ArrayList<FlowerPack> bucket = new ArrayList<>();
+
+    /**
+     * add flowers
+     */
     public void add(FlowerPack flowerPack){
         bucket.add(flowerPack);
     }
 
+    /**
+     * get price of flower
+     */
     public int getPrice(){
         int sum = 0;
         for (FlowerPack flowerPack: bucket){

872f048f7ab31b2a95dc8021c153cdda243658d6
KateKo04 <kovalkatty469@gmail.com>
2022-10-20 10:44:17 +0300

    message

--- src/main/java/org/example/Flower.java
+++ src/main/java/org/example/Flower.java
@@ -5,14 +5,24 @@ import lombok.Setter;
 
 @Setter
 public class Flower {
+
+    /**
+     * Take length of flower's sepal
+     */
     @Getter
     private double sepalLength;
+
+    /**
+     * Take color of flower
+     */
     @Getter
     private FlowerColor color;
+
+    /**
+     * Take price of flower
+     */
     @Getter
     private double price;
-
-    private FlowerType flowerType;
 
 }
 

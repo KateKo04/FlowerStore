11d3e8eb6bfb160abacdc90a886e9d4f0aa365c9
KateKo04 <kovalkatty469@gmail.com>
2022-10-20 10:52:59 +0300

    message

--- src/main/java/org/example/FlowerPack.java
+++ src/main/java/org/example/FlowerPack.java
@@ -1,17 +1,31 @@
 package org.example;
 
-import java.util.ArrayList;
-
+/**
+ * pack of flowers
+ */
 public class FlowerPack {
 
+    /**
+     * flower
+     */
     private Flower flower;
+
+    /**
+     * amount of flowers
+     */
     private int amount;
 
+    /**
+     * initialize pack of flowers
+     */
     public FlowerPack(Flower flower, int amount){
         this.flower = flower;
         this.amount = amount;
     }
 
+    /**
+     * get price of flower pack
+     */
     public double getPrice(){
         return flower.getPrice() * amount;
     }

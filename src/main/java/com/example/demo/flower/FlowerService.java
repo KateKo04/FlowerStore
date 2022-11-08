859d72a73af06147da32d8962b9fe4d077204a39
KateKo04 <65810146+KateKo04@users.noreply.github.com>
2022-11-08 19:48:56 +0200

    Add files via upload

--- src/main/java/com/example/demo/flower/FlowerService.java
+++ src/main/java/com/example/demo/flower/FlowerService.java
@@ -1,5 +1,6 @@
 package com.example.demo.flower;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,14 @@ import java.util.List;
 
 @Service
 public class FlowerService {
+
+    @Autowired
+    private FlowerRepository flowerRepository;
     public List<Flower> getFlowers(){
-        return List.of(new Rose(), new Chamomile(), new Flower());
+        return flowerRepository.findAll();
+    }
+
+    public void addFlower(Flower flower){
+        flowerRepository.save(flower);
     }
 }
